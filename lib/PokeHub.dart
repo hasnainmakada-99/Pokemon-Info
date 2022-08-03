f06ad96fb97fbba36a632da3fd39d5ca5d709e13
Hasnain Makada <82728823+hasnainmakada-99@users.noreply.github.com>
2022-08-03 15:01:50 +0530

    Integrated the main UI with Homepage

--- lib/PokeHub.dart
+++ lib/PokeHub.dart
@@ -7,7 +7,7 @@ class PokeHub {
     if (json['pokemon'] != null) {
       pokemon = <Pokemon>[];
       json['pokemon'].forEach((v) {
-        pokemon!.add(new Pokemon.fromJson(v));
+        pokemon?.add(new Pokemon.fromJson(v));
       });
     }
   }
@@ -32,8 +32,8 @@ class Pokemon {
   String? candy;
   int? candyCount;
   String? egg;
-  double? spawnChance;
-  double? avgSpawns;
+  String? spawnChance;
+  String? avgSpawns;
   String? spawnTime;
   List<double>? multipliers;
   List<String>? weaknesses;
@@ -70,10 +70,10 @@ class Pokemon {
     candy = json['candy'];
     candyCount = json['candy_count'];
     egg = json['egg'];
-    spawnChance = json['spawn_chance'];
-    avgSpawns = json['avg_spawns'];
+    spawnChance = json['spawn_chance'].toString();
+    avgSpawns = json['avg_spawns'].toString();
     spawnTime = json['spawn_time'];
-    multipliers = json['multipliers'].cast<double>();
+    multipliers = json['multipliers']?.cast<double>();
     weaknesses = json['weaknesses'].cast<String>();
     if (json['next_evolution'] != null) {
       nextEvolution = <NextEvolution>[];
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pokemon_info/PokeHub.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Pokemon Info',
@@ -36,20 +38,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var url =
-      "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
-
   @override
   void initState() {
     fetchData();
     super.initState();
   }
 
-  late PokeHub pokeHub;
+  PokeHub pokeHub = PokeHub();
   fetchData() async {
+    var url =
+        "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
     var res = await http.get(Uri.parse(url));
     var decodedJson = jsonDecode(res.body);
     pokeHub = PokeHub.fromJson(decodedJson);
+    setState(() {});
   }
 
   @override
@@ -58,14 +60,50 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Pokemon Info'),
-        backgroundColor: Colors.cyan,
+        backgroundColor: Color.fromARGB(255, 22, 123, 224),
       ),
-      drawer: Drawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        backgroundColor: Colors.cyan,
+        backgroundColor: Color.fromARGB(255, 22, 123, 224),
         child: const Icon(Icons.refresh),
       ),
+      body: pokeHub.pokemon == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : GridView.count(
+              crossAxisCount: 2,
+              children: pokeHub.pokemon!
+                  .map((poke) => Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: InkWell(
+                          onTap: () {},
+                          child: Card(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Container(
+                                  height: 100.0,
+                                  width: 100.0,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: NetworkImage(poke.img.toString()),
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  poke.name.toString(),
+                                  style: const TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ))
+                  .toList(),
+            ),
     );
   }
 }
